c08cba8dc2384c386d482438f5b9bd9f5760a8fb
Vinicius Souza <viisouza10@live.com>
2020-02-02 18:55:25 -0300

    Configurando MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-
+import mongoose from 'mongoose';
 import Appointment from '../app/models/Appointment';
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -11,6 +11,7 @@ const models = [Appointment, User, File];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -19,6 +20,13 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModift: true }
+    );
   }
 }
 
