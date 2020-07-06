1b3c7541e1aff72ae344d76266f7ced0b5fc8d39
Vinicius Souza <viisouza10@live.com>
2020-07-06 12:44:50 -0300

    Criando repositórios

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,21 +1,26 @@
+import { parseISO, startOfHour } from 'date-fns';
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository()
 
-const appointments = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
-  const appointment = {
-    id: uuid(),
-    provider,
-    date
+
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
   }
 
-  appointments.push(appointment)
+  const appointment = appointmentsRepository.create(provider, parsedDate)
 
-  return response.json(appointment)
-})
+  return response.json(appointment);
+});
 
 export default appointmentsRouter;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,8 +5,6 @@ const routes = Router();
 routes.use(express.json())
 
 routes.use('/appointments', appointmentsRouter)
-routes.get('/', (request, response) => {
-  return response.json("opa")
-})
+
 
 export default routes;
